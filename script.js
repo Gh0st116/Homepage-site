cd373b8afbd8da763d4f12f5baec88cd68ad8b7d
Gh0st116 <66926881+Gh0st116@users.noreply.github.com>
2022-05-28 00:23:10 -0300

    Uploading files directly from CS50's codespace
    Known bug: theme icon not changing when theme is changed.

---
+++ script.js
@@ -0,0 +1,7 @@
+let icon = document.getElementById("icon-toggle");
+
+// When clicked, toggle theme and change its icon
+icon.addEventListener("click", function () {
+    document.body.classList.toggle("dark-theme");
+
+});
